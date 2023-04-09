5e26add8dc7f7856b198e204cdc12c546f38a30e
sajedaalrababah <sajedaalrababah@icloud.com>
2023-04-09 20:16:34 +0000

    p

--- code-challenges/challenges-18.js
+++ code-challenges/challenges-18.js
@@ -75,6 +75,19 @@ const wordLetters = (str1, str2) => {
 
 const targetIndex = (arr, int) => {
     // write your code here
+    let left = 0;
+    let right = arr.length - 1;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (arr[mid] === int) {
+            return mid;
+        } else if (arr[mid] < int) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return left;
 }
 // -------------------------------------------------------------------------------------------------------
 
